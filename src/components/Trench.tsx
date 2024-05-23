bc74f6003343aba6fe4e678c4815027451404e43
sean <seanyoung247@gmail.com>
2024-05-23 09:11:28 +0100

    feat: Adds initial trench floor texture

--- src/components/Trench.tsx
+++ src/components/Trench.tsx
@@ -1,7 +1,10 @@
 import './Trench.css'
+import floorTex from '../assets/floor.webp'
 
 export const Trench = () => (
-    <div className="floor">
+    <div className="floor" style={{
+        '--floor': `url(${floorTex})`
+    }}>
         <div className="left"></div>
         <div className="right"></div>
     </div>
