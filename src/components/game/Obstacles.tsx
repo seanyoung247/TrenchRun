058896be17d506e33618bc294f3d69f7f9de5c39
Sean Young <seanyoung247@gmail.com>
2024-05-26 11:28:30 +0100

    refactor: Improves obstacle positioning and generation

--- src/components/game/Obstacles.tsx
+++ src/components/game/Obstacles.tsx
@@ -1,8 +1,8 @@
 
-import { ReactElement, useEffect, useRef } from 'react'
+import { ReactElement, useRef } from 'react'
 
 import Player from '../../types/player'
-import { generateLayout } from '../../types/obstacle'
+import { ObstacleLayout, generateLayout } from '../../types/obstacle'
 import { classList } from '../../util/css'
 
 import './Obstacles.css'
@@ -10,28 +10,54 @@ import './Obstacles.css'
 type ObstacleProps = {
     player: Player,
     start?: number,
+    onCollision?: (()=>void) | null,
 }
 
-export const Obstacle = ({player, start=0}:ObstacleProps) => {
+type ObstacleData = {
+    position: number,
+    segments: ReactElement[],
+    layout: ObstacleLayout,
+}
+
+const genSegments = (layout:ObstacleLayout) => layout.map(v => (
+    <div className={ classList("segment", (v === 1)&&"filled") }/> 
+))
+
+const newObstacleData = (
+    position=0, 
+    layout:ObstacleLayout=[0,0,0, 0,0,0, 0,0,0], 
+    segments:ReactElement[]=genSegments(layout)
+):ObstacleData => ({position, segments, layout})
+
+export const Obstacle = ({player, start=0, onCollision=null}:ObstacleProps) => {
     const firstPos = player.position
     const lastPos = firstPos + 20
-    const position = useRef(firstPos + start)
-    const layout = useRef<ReactElement[]>([])
+    const descriptor = useRef<ObstacleData>(newObstacleData(firstPos + start))
 
-    if (position.current < firstPos) {
+    // If the obstacle is behind the player:
+    // remove it and create a new one at the end of the trench
+    if (descriptor.current.position + 2 < firstPos) {
         const layoutTemplate = generateLayout()
-        layout.current = layoutTemplate.map(v => (
-            <div className={ classList("segment", (v === 1)&&"filled") }/>
-        ))
+        descriptor.current = newObstacleData(
+            lastPos, layoutTemplate, genSegments(layoutTemplate)
+        )
+    }
 
-        position.current = lastPos
+    if (onCollision !== null) {
+        // Player is always at first position, so there can 
+        // only be a collision if the obstacle is at firstPos
+        if (Math.floor(descriptor.current.position) === Math.floor(firstPos)) {
+            // Player has collided if the obstacle segment at 
+            // the same x,y offset as the player is filled
+
+        }
     }
 
     return (
         <div className="obstacle" style={{
-            '--zPos': lastPos - position.current 
+            '--zPos': lastPos - descriptor.current.position 
         }}>
-            { layout.current }
+            { descriptor.current.segments }
         </div>
     )
 }
@@ -43,19 +69,13 @@ type ObstacleListProps = {
 }
 
 export const Obstacles = ({player, count=1, onCollision=null}:ObstacleListProps) => {
-    const obstacles = useRef<ReactElement[]>()
-    useEffect(() => {
-        obstacles.current = []
-        for (let i = 0; i < count; i++) {
-            obstacles.current.push(
-                <Obstacle key={i} player={player} start={(i * 5) + 10}/>
-            )
-        }
-    }, [count, player])
-
-    if (onCollision !== null) {
-        // Do collision detection
+    const obstacles = []
+    for (let i = 0; i < count; i++) {
+        const start = (i * 5) + 10
+        obstacles.push(
+            <Obstacle key={i} player={player} start={start} onCollision={onCollision}/>
+        )
     }
 
-    return <>{ obstacles.current }</>
+    return <>{ obstacles }</>
 }
